fb0b08bfa7e0e4989a98cc438ec33dfb171911d3
ntwigamartin <ntwiga.martin@gmail.com>
2023-02-15 15:46:38 +0300

    Completed, all tests passed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+# bob_attributes = { name: "Bob", hair_color: "Brown" }
+# bob = Person.new(bob_attributes)
+# pp bob.name       
+# pp bob.hair_color
+
+# susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+# susan = Person.new(susan_attributes)
+# pp susan.name      
+# pp susan.height    
+# pp susan.eye_color
